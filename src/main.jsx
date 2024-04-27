809d2496fb910a80d64ed9fd7af632c9e91e3fb2
Valerio Varvaglione <vvalerio94@hotmail.it>
2024-04-27 15:59:23 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://bb49f19a77b14b278269b116d37b393e@o4507154522046464.ingest.de.sentry.io/4507154527813712",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+    
+  ],
+  
+  tracesSampleRate: 1.0, 
+  
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
